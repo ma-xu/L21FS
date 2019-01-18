bf92837f1ec116de11bde42e3124a6f3e20b3bfb
13952522076 <melody@dynamic-10-125-156-122.dynamic.unt.edu>
2019-01-17 19:04:41 -0600

    aaa

---
+++ 0V2/L21FS/experiments/plot_selec_trans.m
@@ -0,0 +1,18 @@
+clear;
+clc;
+load('testCorrList_transformation.mat')
+load('testCorrList_selection.mat')
+y = 80:10:220;
+plot(y,testCorrList_transformation(1,8:22)/100,'-*r');
+hold on;
+plot(y,testCorrList_selection(1,8:22)/100,'-ob');
+xlim([80 220]);
+ylim([0.85 0.94]);
+set(gca,'FontSize',12);
+xlabel('Selected feature numbers','FontSize',14);
+ylabel('Accuracy','FontSize',14);
+legend({'Transformation','Selection'},'FontSize',16)
+
+
+
+
