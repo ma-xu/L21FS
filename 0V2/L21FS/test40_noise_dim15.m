548af459ae9bee9f28cb6c70c25785aef14119b2
Xu Ma <xuma@my.unt.edu>
2019-01-26 12:17:44 -0600

    noise data

---
+++ 0V2/L21FS/test40_noise_dim15.m
@@ -0,0 +1,34 @@
+clear;
+clc;
+DataName={'lung_discrete';'colon';'lung';'mnist'};
+%DataName={'colon';'lung';'warpPIE10P';'mnist'};
+rate=40;
+outratio=0.3;
+DataNum=size(DataName,1);
+for i=1:DataNum
+    datapath=['../../data/',DataName{i},'.mat'];
+    if ~exist([num2str(rate),'Data_dim15'],'dir') 
+        mkdir([num2str(rate),'Data_dim15']);
+    end 
+    savePath=[num2str(rate),'Data_dim15/Data_',DataName{i},'.mat'];
+    
+    if outratio >0
+        savetemp=[num2str(rate),'Data_dim15_',num2str(outratio),'Noise'];
+        if ~exist(savetemp,'dir') 
+            mkdir(savetemp);
+        end 
+        savePath=[savetemp,'/Data_',DataName{i},'.mat'];
+    end
+    
+    load(datapath);
+    B=[gnd fea];
+    feaNum=rate;
+    rdim=15;
+    %data=instance( B,feaNum,rdim);
+    Name = ['Data_',DataName{i}];
+    eval([Name,'=instance1( B,feaNum,rdim,outratio);']);
+    eval(['save(savePath,','''',Name,''');']);
+    clearvars -except DataName rate DataNum outratio;
+    clc;
+end
+
--- 0V2/runTest.m
+++ 0V2/runTest.m
@@ -1,220 +1,25 @@
 
 try
-    clear;clc;run('L21FS/test20_dim10');
+    clear;clc;run('L21FS/test20_noise_dim7');
 catch ErrorInfo
-    write_error('URAFS/test20_dim10',ErrorInfo);
+    write_error('L21FS/test20_noise_dim7',ErrorInfo);
 end
 try
-    clear;clc;run('L21FS/test20_dim20');
+    clear;clc;run('L21FS/test20_noise_dim15');
 catch ErrorInfo
-    write_error('URAFS/test20_dim20',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test20_dim40');
-catch ErrorInfo
-    write_error('URAFS/test20_dim40',ErrorInfo);
-end
-
-
-
-
-try
-    clear;clc;run('L21FS/test40_dim10');
-catch ErrorInfo
-    write_error('URAFS/test40_dim10',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test40_dim20');
-catch ErrorInfo
-    write_error('URAFS/test40_dim20',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test40_dim40');
-catch ErrorInfo
-    write_error('URAFS/test40_dim40',ErrorInfo);
-end
-
-
-
-
-try
-    clear;clc;run('L21FS/test20_noise_dim10');
-catch ErrorInfo
-    write_error('URAFS/test20_noise_dim10',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test20_noise_dim20');
-catch ErrorInfo
-    write_error('URAFS/test20_noise_dim20',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test20_noise_dim40');
-catch ErrorInfo
-    write_error('URAFS/test20_noise_dim40',ErrorInfo);
-end
-
-
-try
-    clear;clc;run('L21FS/test40_noise_dim10');
-catch ErrorInfo
-    write_error('URAFS/test40_noise_dim10',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test40_noise_dim20');
-catch ErrorInfo
-    write_error('URAFS/test40_noise_dim20',ErrorInfo);
-end
-try
-    clear;clc;run('L21FS/test40_noise_dim40');
-catch ErrorInfo
-    write_error('URAFS/test40_noise_dim40',ErrorInfo);
-end
-%{
-try
-    clear;clc;run('URAFS/test');
-catch ErrorInfo
-    write_error('URAFS/test',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test20_Noise');
-catch ErrorInfo
-    write_error('URAFS/test20_Noise',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test40');
-catch ErrorInfo
-    write_error('URAFS/test40',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test40_Noise');
-catch ErrorInfo
-    write_error('URAFS/test40_Noise',ErrorInfo);
-end
-%}
-
-
-%{
-try
-    clear;clc;run('LDFS/test20');
-catch ErrorInfo
-    write_error('LDFS/test20',ErrorInfo);
+    write_error('L21FS/test20_noise_dim15',ErrorInfo);
 end
 
 try
-    clear;clc;run('LDFS/test20_noise');
+    clear;clc;run('L21FS/test40_noise_dim7');
 catch ErrorInfo
-    write_error('LDFS/test20_noise',ErrorInfo);
-end
-
-try
-    clear;clc;run('LDFS/test40_noise');
-catch ErrorInfo
-    write_error('LDFS/test40_noise',ErrorInfo);
-end
-
-%--------LS----------
-try
-    clear;clc;run('LS/test20_big');
-catch ErrorInfo
-    write_error('LS/test20_big',ErrorInfo);
+    write_error('L21FS/test40_noise_dim7',ErrorInfo);
 end
 try
-    clear;clc;run('LS/test20_big_noise');
+    clear;clc;run('L21FS/test40_noise_dim15');
 catch ErrorInfo
-    write_error('LS/test20_big_noise',ErrorInfo);
+    write_error('L21FS/test40_noise_dim15',ErrorInfo);
 end
-try
-    clear;clc;run('LS/test40_big_noise');
-catch ErrorInfo
-    write_error('LS/test40_big_noise',ErrorInfo);
-end
-
-%---------MCFS----------
-try
-    clear;clc;run('MCFS/test20_big');
-catch ErrorInfo
-    write_error('MCFS/test20_big',ErrorInfo);
-end
-try
-    clear;clc;run('MCFS/test20_big_noise');
-catch ErrorInfo
-    write_error('MCFS/test20_big_noise',ErrorInfo);
-end
-try
-    clear;clc;run('MCFS/test40_big_noise');
-catch ErrorInfo
-    write_error('MCFS/test40_big_noise',ErrorInfo);
-end
-
-
-%--------------UMM---------------
-
-try
-    clear;clc;run('UMM/test20_big');
-catch ErrorInfo
-    write_error('UMM/test20_big',ErrorInfo);
-end
-try
-    clear;clc;run('UMM/test20_big_noise');
-catch ErrorInfo
-    write_error('UMM/test20_big_noise',ErrorInfo);
-end
-try
-    clear;clc;run('UMM/test40_big_noise');
-catch ErrorInfo
-    write_error('UMM/test40_big_noise',ErrorInfo);
-end
-
-%--------------DFS---------------
-
-try
-    clear;clc;run('DFS/test20_big');
-catch ErrorInfo
-    write_error('DFS/test20_big',ErrorInfo);
-end
-try
-    clear;clc;run('DFS/test40_big');
-catch ErrorInfo
-    write_error('DFS/test40_big',ErrorInfo);
-end
-try
-    clear;clc;run('DFS/test20_big_noise');
-catch ErrorInfo
-    write_error('DFS/test20_big_noise',ErrorInfo);
-end
-try
-    clear;clc;run('DFS/test40_big_noise');
-catch ErrorInfo
-    write_error('DFS/test40_big_noise',ErrorInfo);
-end
-
-%--------------URAFS---------------
-try
-    clear;clc;run('URAFS/test20_big');
-catch ErrorInfo
-    write_error('URAFS/test20_big',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test40_big');
-catch ErrorInfo
-    write_error('URAFS/test40_big',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test20_big_noise');
-catch ErrorInfo
-    write_error('URAFS/test20_big_noise',ErrorInfo);
-end
-try
-    clear;clc;run('URAFS/test40_big_noise');
-catch ErrorInfo
-    write_error('URAFS/test40_big_noise',ErrorInfo);
-end
-%}
-
-
-
-
-
 
 
 
